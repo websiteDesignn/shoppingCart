fbf9e7c373788d7da8153b0fec18d452a30e815e
Taha <149267110+websiteDesignn@users.noreply.github.com>
2024-05-01 14:17:45 +0330

    new

--- util/addGlobalEventListener.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-export default function addGlobalEventListener(type, selector, callback) {
-  document.addEventListener(type, (e) => {
-    if (e.target.matches(selector)) {
-      callback(e);
-    }
-  });
-}
